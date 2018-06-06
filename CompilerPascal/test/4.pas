86207d76acc5a372ce5ca0efd973ce7405741047
feiba15 <470603383@qq.com>
2018-06-06 09:43:26 +0800

    更新

--- CompilerPascal/test/4.pas
+++ CompilerPascal/test/4.pas
@@ -2,8 +2,10 @@
         const a = 1;
               b = 2;
               c = 1.0;
-        var a, b, c:real;
+        var      
+        		a, b, c: real;
             x, y:integer;
         Begin 
-        	x := 1;               
+        	x := 1;  
+        	a := 1;        
         End.
